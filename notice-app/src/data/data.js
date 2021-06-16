eccee6bf391f45e6de1ab0df18366d48caa61d26
devangelfish <jaebeommun@gmail.com>
2021-06-16 23:16:57 +0900

    init: react-create-app, notice-app 생성

---
+++ notice-app/src/data/data.js
@@ -0,0 +1,55 @@
+let data = [
+    {
+        title: '심사내역',
+        list: [
+            {
+                url: '#',
+                description: "2021년 심사 HOT ISSUE"
+            },
+            {
+                url: '#',
+                description: "2020년 심사 HOT ISSUE"
+            },
+            {
+                url: '#',
+                description: "2019년 심사 HOT ISSUE"
+            }
+        ]
+    },
+    {
+        title: '프로그램내역',
+        list: [
+            {
+                url: '#',
+                description: "2021년 업데이트 HOT ISSUE"
+            },
+            {
+                url: '#',
+                description: "2020년 업데이트 HOT ISSUE"
+            },
+            {
+                url: '#',
+                description: "2019년 업데이트 ISSUE"
+            }
+        ]
+    },
+    {
+        title: '기타내역',
+        list: [
+            {
+                url: '#',
+                description: "2021년 기타내역"
+            },
+            {
+                url: '#',
+                description: "2020년 기타내역"
+            },
+            {
+                url: '#',
+                description: "2019년 기타내역"
+            }
+        ]
+    }
+]
+
+export default data
