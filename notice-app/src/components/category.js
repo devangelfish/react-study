a48380d927a2f539bcf8fe7d6aea3bd5f43d71a8
devangelfish <jaebeommun@gmail.com>
2021-07-13 08:57:24 +0900

    update: callback 함수 인라인

--- notice-app/src/components/category.js
+++ notice-app/src/components/category.js
@@ -8,10 +8,8 @@ import './css/category.css'
 const Category = ({category}) => {
     const [ isFold, setIsFold ] = useState(false)
 
-    const onClickArrow = useCallback(() => setIsFold(!isFold), [isFold])
-
     return (<li>
-        <div id='category' key={category.no}>{category.name}<FontAwesomeIcon className='anim' id='up-arrow' style={isFold ? {transform: 'rotate(180deg)'}: null} icon={faAngleDoubleUp} onClick={onClickArrow} /></div>
+        <div id='category' key={category.no}>{category.name}<FontAwesomeIcon className='anim' id='up-arrow' style={isFold ? {transform: 'rotate(180deg)'}: null} icon={faAngleDoubleUp} onClick={() => setIsFold(!isFold)} /></div>
         <Writing writing={category.writing} fold={isFold} />
     </li>)
 }
