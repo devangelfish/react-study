2a035f10fad0e64c63ad15c99c7d23e03fd7f3f3
devangelfish <jaebeommun@gmail.com>
2021-07-13 10:03:57 +0900

    클래스 컴포넌트 주석 삭제

--- notice-app/src/components/writing.js
+++ notice-app/src/components/writing.js
@@ -1,34 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react'
 import './css/writing.css'
 
-// class Writing extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             height: 0,
-//         }
-//     }
-
-//     componentDidMount() {
-//         this.setState({
-//             height: this.box.clientHeight
-//         })
-//     }
-
-//     render() {
-//         const { writing, fold } = this.props
-//         const { height } = this.state
-//         return (
-//             <div id="writing" className={fold ? "closed-height anim" : "anim"}
-//                 style={height === 0 ? null : { 'height': height }} ref={ref => this.box = ref}>
-//                 {writing.map(({ no, title }) =>
-//                     <div key={no} id="writing-title" className='ellipsis'>{title}</div>
-//                 )}
-//             </div>
-//         )
-//     }
-// }
-
 const Writing = ({ writing, fold }) => {
     const [ height, setHeight ] = useState(0)
     const writingEl = useRef()
